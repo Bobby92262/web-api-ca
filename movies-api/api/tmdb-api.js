7d0ecb1ab01ce10f75d7826f0b9ec119bc22abe4
Bobby92262 <bobbyfinn086@gmail.com>
2025-12-17 09:56:31 +0000

    Backend fetch requests for previous TMDB fetch calls.

--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -1,8 +1,139 @@
 import fetch from 'node-fetch';
 
+//Discover movies
 export const getMovies = async () => {
     const response = await fetch(
         `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    );
+
+    if (!response.ok) {
+        throw new Error(response.json().message);
+    }
+
+    return await response.json();
+};
+
+// Single movie details
+export const getMovie = async (id) => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    );
+
+    if (!response.ok) {
+        throw new Error(response.json().message);
+    }
+
+    return await response.json();
+};
+
+// Upcoming movies
+export const getUpcomingMovies = async () => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    );
+
+    if (!response.ok) {
+        throw new Error(response.json().message);
+    }
+
+    return await response.json();
+};
+
+// Genres
+export const getGenres = async () => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    );
+
+    if (!response.ok) {
+        throw new Error(response.json().message);
+    }
+
+    return await response.json();
+};
+
+// Movie Image
+export const getMovieImages = async (id) => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    );
+
+    if (!response.ok) {
+        throw new Error(response.json().message);
+    }
+
+    return await response.json();
+};
+
+// Movie Credits
+export const getMoviesCredits = async (id) => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    );
+
+    if (!response.ok) {
+        throw new Error(response.json().message);
+    }
+
+    return await response.json();
+};
+
+//Similar Movies
+export const getSimilarMovies = async (id) => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    );
+
+    if (!response.ok) {
+        throw new Error(response.json().message);
+    }
+
+    return await response.json();
+};
+
+// Trending movies (week)
+export const getTrendingMovies = async () => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/trending/week?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    );
+
+    if (!response.ok) {
+        throw new Error(response.json().message);
+    }
+
+    return await response.json();
+};
+
+// Top rated
+export const getTopRatedMovies = async () => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    );
+
+    if (!response.ok) {
+        throw new Error(response.json().message);
+    }
+
+    return await response.json();
+};
+
+//Person details
+export const getPersonDetails = async (id) => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/person/${id}?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    );
+
+    if (!response.ok) {
+        throw new Error(response.json().message);
+    }
+
+    return await response.json();
+};
+
+//Person Images
+export const getPersonImages = async (id) => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/person/${id}/images?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
     );
 
     if (!response.ok) {
